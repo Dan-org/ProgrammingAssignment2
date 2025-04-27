8c150209bb9040f1b3c47c57abe5e21ba217392d
Dan-org <daniel.rees.lewis@gmail.com>
2025-04-27 15:13:41 +0200

    First effort good enough to commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+#### makeCacheMatrix & cacheSolve Functions
+## Why do this? This is an example of caching
 
-## Edit to see if pushing and committing works
 
+### Define makeCacheMatrix
+# Function that creates a matrix object that can cache it's inverse
 makeCacheMatrix <- function(x = matrix()) {
+# No inverse matrix calculated initially    
+    i <- NULL
 
+# Assign new matrix, y, to x  
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+
+# Get x from parent environment
+  get <- function() x
+
+# Updates i in parent envionment
+  setinverse <- function(inverse) i <<- inverse
+
+# Gets the cached inverse
+  getinverse <- function() i
+  
+# Creates list of functions 
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
 
+### Define cacheSolve
+# Function that looks to cache the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+# Gets cached inverse if exists
+    i <- x$getinverse()
+
+    
+    if(!is.null(i)) # Checks if it exists
+  {
+    message("winner—getting cached data :)")
+    return(i) # prints message if gets cached and doesn't run new calculation
+  }
+  data <- x$get() # if no cache then calculate
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+
+# Create a random matrix
+my_matrix <- matrix(c(2, 1, 1, 2), nrow = 2, ncol = 2)
+
+# Create cached matrix object
+cachedMatrix <- makeCacheMatrix(my_matrix)
+
+# First time solving: will calculate
+cacheSolve(cachedMatrix)
+
+# Second time solving: will use cache! Winner :) 
+cacheSolve(cachedMatrix)
